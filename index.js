99beb325fa889f15252efcaaa2037af4bf8abe86
sbussetti <steve.bussetti@gmail.com>
2017-02-08 10:57:33 -0500

    initial

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // AssetRebaseWebpackPlugin.js
+var path = require('path');
 
 function AssetRebaseWebpackPlugin(options) {
+  this.options = Object.assign({
+    relative: false,
+  }, options || {});
 }
 
 AssetRebaseWebpackPlugin.prototype.apply = function(compiler) {
   // ...
+  var options = this.options;
   compiler.plugin('compilation', function(compilation) {
     compilation.plugin('html-webpack-plugin-alter-asset-tags', function(htmlPluginData, callback) {
-      console.log(arguments);
+      [htmlPluginData.body, htmlPluginData.head].forEach(function(region) {
+        region.forEach(function(tag) {
+          ['src', 'href'].forEach(function(attr) {
+            var val = tag.attributes[attr];
+            if (!val) { return; }
+
+            if (options.relative === true) {
+              tag.attributes[attr] = path.basename(val);
+            } else if(typeof options.relative === 'string') {
+              tag.attributes[attr] = path.relative(options.relative, path.resolve(val));
+            }
+          });
+        });
+      });
       callback(null, htmlPluginData);
     });
   });
